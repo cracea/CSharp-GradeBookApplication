5d7b4e27db6d177f3c662f5cbd7f8a48d57c5e48
Ion Cracea <ioncracea@asdan.org.uk>
2019-04-08 14:06:40 +0100

    Completed the module

--- GradeBook/RankedGradeBook.cs
+++ GradeBook/RankedGradeBook.cs
@@ -1,16 +1,14 @@
-﻿using GradeBook.Enums;
-using System;
+﻿using System;
 using System.Linq;
-
 namespace GradeBook.GradeBooks
 {
-    public class RakedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
-        public RakedGradeBook(string name) : base(name)
+        public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
-            Type = GradeBookType.Ranked;
+            Type = Enums.GradeBookType.Ranked;
         }
-        
+
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
@@ -18,9 +16,8 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException("You must have at least 5 students to do ranked grading.");
             }
 
-            int threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
-
 
             if (averageGrade >= grades[threshold - 1])
                 return 'A';
@@ -30,10 +27,29 @@ namespace GradeBook.GradeBooks
                 return 'C';
             if (averageGrade >= grades[(threshold * 4) - 1])
                 return 'D';
-            return 'F'; ;
+            return 'F';
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count() < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
 
+            base.CalculateStatistics();
+        }
 
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count() < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
